f1ca026177790b3f9f56569e6e83972ff11afde6
NoorTushar <tushar.noor.khan@gmail.com>
2024-07-15 05:29:00 +0600

    testmonial slider just added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner/Banner";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 const Home = () => {
    return (
       <>
          <Banner />
+         <Testimonials />
       </>
    );
 };
