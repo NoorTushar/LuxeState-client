2e4044f1973b4f36617743f6892a6965caeb6f50
NoorTushar <tushar.noor.khan@gmail.com>
2024-08-06 13:28:09 +0600

    reworked card spacing in the services page

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -4,12 +4,12 @@ const ServiceCard = ({ service }) => {
    return (
       <li>
          <div className="bg-neutral-900 p-10 space-y-4 flex flex-col min-h-min h-full">
-            <h2 className="text-3xl tracking-[2px] uppercase text-ourPrimary flex-grow">
+            <h2 className="text-3xl tracking-[2px] uppercase text-ourPrimary flex-1">
                {service.title}
             </h2>
-            <p className="flex-grow">{service.description}</p>
+            <p className="">{service.description}</p>
 
-            <div className="">
+            <div className="flex-grow flex items-end justify-">
                <Link className="py-1.5 px-4 relative group overflow-hidden font-medium  inline-block custom-next border-ourDeeperGold border hover:border-black bg-ourDeeperGold text-white uppercase">
                   <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-neutral-900 group-hover:h-full opacity-90"></span>
                   <span className="relative tracking-[2px] text-sm">
